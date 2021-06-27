cd6f8dee0e6a5713acff7c2249dc2ad3a115dfbd
chenxi <chenxi.et@gmail.com>
2021-06-27 23:06:57 +0800

    ch2-ioc容器概览 依赖查找，按类型查找集合bean对象

--- ioc-container-overview/src/main/java/io/github/cchenxi/spring/core/ioc/overview/dependency/lookup/DependencyLookupDemo.java
+++ ioc-container-overview/src/main/java/io/github/cchenxi/spring/core/ioc/overview/dependency/lookup/DependencyLookupDemo.java
@@ -2,8 +2,11 @@ package io.github.cchenxi.spring.core.ioc.overview.dependency.lookup;
 
 import io.github.cchenxi.spring.core.ioc.overview.domain.User;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Map;
 
 /**
  * 依赖查找示例
@@ -19,6 +22,7 @@ public class DependencyLookupDemo {
 
         lookupInRealTime(beanFactory);
         lookupInLazy(beanFactory);
+        lookupCollectionByType(beanFactory);
     }
 
     private static void lookupInRealTime(BeanFactory beanFactory) {
@@ -30,5 +34,13 @@ public class DependencyLookupDemo {
         ObjectFactory<User> objectFactory = (ObjectFactory<User>) beanFactory.getBean("objectFactory");
         User user = objectFactory.getObject();
         System.out.println("延时查找：" + user);
+    }
+
+    private static void lookupCollectionByType(BeanFactory beanFactory) {
+        if (beanFactory instanceof ListableBeanFactory) {
+            ListableBeanFactory listableBeanFactory = (ListableBeanFactory) beanFactory;
+            Map<String, User> userMap = listableBeanFactory.getBeansOfType(User.class);
+            System.out.println("按类型查找集合 User 对象：" + userMap);
+        }
     }
 }
